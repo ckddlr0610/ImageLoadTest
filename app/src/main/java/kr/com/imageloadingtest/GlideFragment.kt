c17b9e448e3a6f41a7e677c6941c22fe452fdf20
ChangikChoe <46737716+ckddlr0610@users.noreply.github.com>
2020-11-26 18:00:35 +0900

    add Glide Fragment Layout

--- app/src/main/java/kr/com/imageloadingtest/GlideFragment.kt
+++ app/src/main/java/kr/com/imageloadingtest/GlideFragment.kt
@@ -15,7 +15,7 @@ class GlideFragment : Fragment() {
             savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false)
+        return inflater.inflate(R.layout.fragment_glide, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
